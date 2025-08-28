3529dc69f8588c56dd42b038c7cf9ef2cb0580b0
abhishek <abhishek.bahuguna14@gmail.com>
2025-08-28 19:14:01 +0530

    Blog Project

---
+++ 13blogProject/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import conf from './conf/conf'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1>Hello</h1>
+    </>
+  )
+}
+
+export default App
