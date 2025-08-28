e787546f0dd07defbaa929c24a50d17b73e9de69
abhishek <abhishek.bahuguna14@gmail.com>
2025-08-29 01:17:46 +0530

    Blog project added store

--- 13blogProject/src/App.jsx
+++ 13blogProject/src/App.jsx
@@ -1,15 +1,36 @@
-import { useState } from 'react'
-import conf from './conf/conf'
+import { useState, useEffect } from 'react'
+import {useDispatch} from 'react-redux'
+import authService from './appwrite/auth'
+import { login,logout } from './store/authSlice'
+import { Header, Footer } from './components'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(true)
+  const dispatch= useDispatch()
 
-  return (
-    <>
-     <h1>Hello</h1>
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData)
+        dispatch(login({userData}))
+      else
+        dispatch(logout())
+    })
+    .finally(()=>setLoading(false))
+  }, [])
+  
+  return !loading? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ): null
 }
 
 export default App
